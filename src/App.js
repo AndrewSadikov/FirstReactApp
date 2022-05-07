df47a061d385acc93ae4c88a89001034f25cd950
andrewsadikov <me@andrewsadikov.com>
2022-05-07 00:01:33 -0400

    Added photos by link. Removed Footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import Footer from "./components/Footer";
 
 export default function App() {
   return (
@@ -10,14 +9,13 @@ export default function App() {
       <Navbar />
       <Hero />
       <Card
-        img={require("./images/katie-runner.png")}
+        img="https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
         title="Life lessons with Katie Zaferes"
         country="USA"
         rating={4.7}
         reviewCount={8}
         cost={136}
       />
-      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer>
-      <img src={require("../images/airbnb-logo.png")} className="nav--logo" />
-    </footer>
-  );
-}
