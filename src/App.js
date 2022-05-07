01c3d3648cd4f710505c313056ccf204c29ad5b3
andrewsadikov <me@andrewsadikov.com>
2022-05-07 00:48:19 -0400

    Mapped data to dynamically display in components

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
+
+console.log(data);
 
 export default function App() {
+  const cards = data.map((item) => {
+    return (
+      <Card
+        img={item.img}
+        title={item.title}
+        country={item.country}
+        rating={item.rating}
+        reviewCount={item.reviewCount}
+        cost={item.cost}
+      />
+    );
+  });
+
   return (
     <div>
       <Navbar />
       <Hero />
-      <Card
-        img="https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-        title="Life lessons with Katie Zaferes"
-        country="USA"
-        rating={4.7}
-        reviewCount={8}
-        cost={136}
-      />
+      {cards}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    title: "Life lessons with Katie Zaferes",
+    country: "USA",
+    rating: 4.7,
+    reviewCount: 4,
+    cost: 150,
+  },
+  {
+    id: 2,
+    img: "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
+    title: "Why purple is the smartest color",
+    country: "NETHERLANDS",
+    rating: 4.2,
+    reviewCount: 10,
+    cost: 240,
+  },
+  {
+    id: 3,
+    img: "https://media.macphun.com/img/uploads/customer/how-to/579/15531840725c93b5489d84e9.43781620.jpg?q=85&w=1340",
+    title: "Is your garden using facebook all wrong?",
+    country: "MEXICO",
+    rating: 4.9,
+    reviewCount: 6,
+    cost: 120,
+  },
+];
